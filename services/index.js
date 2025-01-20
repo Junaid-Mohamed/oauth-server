8ab8f09b5a7122ecc2202c434d54156b8180dc0a
Mohammed Junaid <junaid7mohamed@gmail.com>
2025-01-20 11:16:24 +0530

    fix: cookie setting fix

--- services/index.js
+++ services/index.js
@@ -3,7 +3,9 @@
 function setSecureCookie(res, token){
     res.cookie('access_token',token,{
         httpOnly: true,
-        maxAge: 60 * 1000,
+        secure: true,
+        sameSite: "none",
+        maxAge: 24 * 60 * 60 * 1000,
     })
     return res;
 }
