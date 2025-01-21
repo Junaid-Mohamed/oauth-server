99305b57d913e607bcc656ff1398b26e4164bc8c
Mohammed Junaid <junaid7mohamed@gmail.com>
2025-01-21 22:36:22 +0530

    feat: jwt added

--- index.js
+++ index.js
@@ -5,11 +5,12 @@ require("dotenv").config();
 const cookieParser = require("cookie-parser")
 const {setSecureCookie} = require("./services/index.js")
 const {verifyAccessToken} = require("./middleware/index.js")
+const jwt = require("jsonwebtoken");
 
 const PORT = process.env.PORT | 4000;
 
 const app = express();
-app.use(cors({credentials: true, origin: "https://oauth-frontend.vercel.app"}));
+app.use(cors({credentials: true, origin: ["https://oauth-frontend.vercel.app","http://localhost:3000"]}));
 app.use(cookieParser());
 
 app.get('/',(req,res)=>{
@@ -19,6 +20,7 @@ app.get('/',(req,res)=>{
 app.get("/user/profile/github",verifyAccessToken ,async(req,res)=>{
     try{
         const {access_token} = req.cookies;
+        const {id,name,email,picture} = jwt.verify(access_token,process.env.JWT_SECRET);
         const githubUserDataResponse = await axios.get('https://api.github.com/user', 
             {
                 headers: {
@@ -35,14 +37,18 @@ app.get("/user/profile/github",verifyAccessToken ,async(req,res)=>{
 app.get("/user/profile/google",verifyAccessToken ,async(req,res)=>{
     try{
         const {access_token} = req.cookies;
-        const googleUserDataResponse = await axios.get('https://www.googleapis.com/oauth2/v2/userinfo', 
-            {
-                headers: {
-                    Authorization: `Bearer ${access_token}`
-                }
-            }
-        )
-        res.json({user:googleUserDataResponse.data});
+        const {id,name,email,picture} = jwt.verify(access_token,process.env.JWT_SECRET);
+        // console.log('Details inside user profile', id,name,email,picture)
+        // res.status(200);
+        // const {access_token} = req.cookies;
+        // const googleUserDataResponse = await axios.get('https://www.googleapis.com/oauth2/v2/userinfo', 
+        //     {
+        //         headers: {
+        //             Authorization: `Bearer ${access_token}`
+        //         }
+        //     }
+        // )
+        res.json({user:{id,name,email,picture}});
     }catch(error){
         res.status(500).json({error: "Could not fetch user Google profile."})
     }
@@ -80,7 +86,7 @@ app.get('/auth/github/callback',async(req,res)=>{
 })
 
 app.get('/auth/google', (req,res)=>{
-    const googleAuthUrl = `https://accounts.google.com/o/oauth2/auth?client_id=${process.env.GOOGLE_CLIENT_ID}&redirect_uri=https://oauth-server-brown.vercel.app/api/auth/google/callback&response_type=code&scope=profile email`
+    const googleAuthUrl = `https://accounts.google.com/o/oauth2/auth?client_id=${process.env.GOOGLE_CLIENT_ID}&redirect_uri=${process.env.BACKEND_URL}/api/auth/google/callback&response_type=code&scope=profile email`
     // res.send("hi");
     res.redirect(googleAuthUrl);
 })
@@ -98,7 +104,7 @@ app.get('/api/auth/google/callback',async(req,res)=>{
             client_secret: process.env.GOOGLE_CLIENT_SECRET,
             code,
             grant_type: "authorization_code",
-            redirect_uri: `https://oauth-server-brown.vercel.app/api/auth/google/callback`
+            redirect_uri: `${process.env.BACKEND_URL}/api/auth/google/callback`
         },
         {
         headers: {
@@ -107,7 +113,6 @@ app.get('/api/auth/google/callback',async(req,res)=>{
     },
     )
     access_token = tokenResponse.data.access_token;
-    setSecureCookie(res,access_token);
     const googleUserDataResponse = await axios.get('https://www.googleapis.com/oauth2/v2/userinfo', 
         {
             headers: {
@@ -115,8 +120,11 @@ app.get('/api/auth/google/callback',async(req,res)=>{
             }
         }
     )
+    const {id,name,email,picture} = googleUserDataResponse.data;
+    const jwtToken = await jwt.sign({id,email,name,picture},process.env.JWT_SECRET,{expiresIn:"12h"})
+    setSecureCookie(res,jwtToken);
     // res.cookie("access_token",access_token);
-    return res.redirect(`https://oauth-frontend.vercel.app/v2/profile/google`)
+    return res.redirect(`${process.env.FRONTEND_URL}/v2/profile/google`)
     }catch(error){
         console.error(error)
     }
